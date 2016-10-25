2733630704a7027922fc811bf23f28cd6221d8ef
ciara <ciarapower88@gmail.com>
2016-10-25 23:57:54 +0100

    Added BruteAutoComplete elements and changed API

--- src/algorithms/AutoCompleteAPI.java
+++ src/algorithms/AutoCompleteAPI.java
@@ -1,10 +1,60 @@
 package algorithms;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class AutoCompleteAPI {
 
-	public static void main(String[] args) {
-		
+	private Scanner input = new Scanner(System.in);
+	private static BruteAutoComplete brute;
+	private int autoCompleteMethod;
 
+	public static void main(String[] args) throws Exception {
+		AutoCompleteAPI app = new AutoCompleteAPI();
+		brute=new BruteAutoComplete();
+		app.run();
+
+	}
+	public AutoCompleteAPI() {
+		input = new Scanner(System.in);
+
+
+	}
+
+	private void run() throws Exception {
+		autoCompleteMethod=CompleteMenu();
+		while(autoCompleteMethod!=3){
+			switch (autoCompleteMethod){
+			case 1: 
+                 brute.bruteRun();
+				break;
+			case 2:
+				break;
+			case 3: System.exit(0);
+			input.close();
+			break;
+			}
+			autoCompleteMethod=CompleteMenu();
+		}
+	}
+	private int CompleteMenu() {
+		System.out.println(" AUTOCOMPLETE PROGRAMS \n---------------------------");
+		System.out.println(" Please choose an option: \n  1) Brute Force AutoComplete \n  2) Quick AutoComplete \n  3) Exit");
+		try {
+			autoCompleteMethod = input.nextInt();
+			while (autoCompleteMethod < 1 || autoCompleteMethod > 3) {   // must get option in the menu range
+				System.out.println("Invalid choice,please choose again!");
+				System.out.print("==>>");
+				autoCompleteMethod = input.nextInt();
+			}
+		} catch (Exception e) {      // if input other than integer is entered
+			System.out.println("---Invalid choice,please choose again!---");
+			System.out.println("    ---Press any key to continue---");
+			input.nextLine();
+			input.nextLine();
+			autoCompleteMethod = CompleteMenu();
+		}
+		return autoCompleteMethod;
 	}
 
 }
---
+++ src/algorithms/BruteAutoComplete.java
@@ -0,0 +1,50 @@
+package algorithms;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BruteAutoComplete {
+
+	private List<Term> terms=new ArrayList<Term>();
+	
+	public void bruteRun() throws Exception{
+		readUrl();
+		System.out.println("..."  );
+		
+	}
+	public BruteAutoComplete() {
+	
+	}
+	public  void readUrl() throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(new URL("https://wit-computing.github.io/algorithms-2016/topic04/book-2/data/wiktionary.txt").openStream()));
+		Scanner inTerm = new Scanner(in);
+		String delims = "[	]";//each field in the file is separated(delimited) by a space.
+		while (inTerm.hasNextLine()) {
+			// get user and rating from data source
+			String termDetails = inTerm.nextLine();
+			// parse user details string
+			String[] termTokens = termDetails.split(delims);
+
+			// output user data to console.
+			if (termTokens.length == 2) {
+				createTerm(termTokens[0],termTokens[1].toLowerCase());
+			}
+			else
+			{
+				if(termTokens.length!=1){
+					inTerm.close();
+					throw new Exception("Invalid member length: "+termTokens.length);
+				}
+			}
+		}
+		inTerm.close();
+	}
+	public void createTerm(String weight,String word){
+		Term term= new Term(weight,word);
+		terms.add(term);
+	}
+}
